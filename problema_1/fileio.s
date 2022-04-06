ac72314b4e528e964040e02316a7bf96925555aa
Gustavo Boanerges <g.boanerges2@gmail.com>
2022-04-06 19:16:57 -0300

    Add main and gpio files

--- problema_1/fileio.s
+++ problema_1/fileio.s
@@ -7,7 +7,7 @@
 .equ O_RDONLY, 0
 .equ O_WRONLY, 1
 .equ O_CREAT, 0100
-.equ S_RDWR, 0666
+.equ S_RDWR, 00002   @0666
 
 .macro openFile fileName, flags
     ldr r0, =\fileName
--- problema_1/gpiomem.s
+++ problema_1/gpiomem.s
@@ -47,6 +47,56 @@
 2:
 .endm
 
+@ Macro nanoSleep to sleep .1 second
+@ Calls Linux nanosleep entry point which is function 162.
+@ Pass a reference to a timespec in both r0 and r1
+@ First is input time to sleep in seconds and nanoseconds.
+@ Second is time left to sleep if interrupted (which we ignore)
+.macro nanoSleep
+    ldr r0, =timespecsec
+    ldr r1, =timespecsec
+    mov r7, #sys_nanosleep
+    svc 0
+.endm
+
+.macro GPIODirectionOut pin
+    ldr r2, =\pin @ offset of select register
+    ldr r2, [r2] @ load the value
+    ldr r1, [r8, r2] @ address of register
+    ldr r3, =\pin @ address of pin table
+    add r3, #4 @ load amount to shift from table
+    ldr r3, [r3] @ load value of shift amt
+    mov r0, #0b111 @ mask to clear 3 bits
+    lsl r0, r3 @ shift into position
+    bic r1, r0 @ clear the three bits
+    mov r0, #1 @ 1 bit to shift into pos
+    lsl r0, r3 @ shift by amount from table
+    orr r1, r0 @ set the bit
+    str r1, [r8, r2] @ save it to reg to do work
+.endm
+
+.macro GPIOTurnOn pin, value
+    mov r2, r8 @ address of gpio regs
+    add r2, #setregoffset @ off to set reg
+    mov r0, #1 @ 1 bit to shift into pos
+    ldr r3, =\pin @ base of pin info table
+    add r3, #8 @ add offset for shift amt
+    ldr r3, [r3] @ load shift from table
+    lsl r0, r3 @ do the shift
+    str r0, [r2] @ write to the register
+.endm
+
+.macro GPIOTurnOff pin, value
+    mov r2, r8 @ address of gpio regs
+    add r2, #clrregoffset @ off set of clr reg
+    mov r0, #1 @ 1 bit to shift into pos
+    ldr r3, =\pin @ base of pin info table
+    add r3, #8 @ add offset for shift amt
+    ldr r3, [r3] @ load shift from table
+    lsl r0, r3 @ do the shift
+    str r0, [r2] @ write to the register
+.endm
+
 .data
 timespecsec: .word 0
 timespecnano: .word 100000000
@@ -57,7 +107,7 @@ memMapErr: .asciz "Failed to map memory\n"
 memMapsz: .word .-memMapErr
  .align 4 @ realign after strings
 @ mem address of gpio register / 4096
-gpioaddr: .word 0x3F200
+gpioaddr: .word 0x3F200 @ 0x7E20100
 pin17: .word 4 @ offset to select register
  .word 21 @ bit offset in select register
  .word 17 @ bit offset in set & clr register
@@ -67,4 +117,4 @@ pin22: .word 8 @ offset to select register
 pin27: .word 8 @ offset to select register
  .word 21 @ bit offset in select register
  .word 27 @ bit offset in set & clr register
-.text
+.text   
