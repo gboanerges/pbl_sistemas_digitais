29459d14df50bad89969af6f7287e43eef7ac1e3
Gustavo Boanerges <g.boanerges2@gmail.com>
2022-04-08 20:48:43 -0300

    Add mapMem macro parameter

--- problema_1/gpiomem.s
+++ problema_1/gpiomem.s
@@ -13,9 +13,9 @@
 .equ MAP_SHARED, 1
 
 @ Macro to map memory for GPIO Registers
-.macro mapMem
+.macro mapMem phys_address
     openFile devmem @ open /dev/mem
-    movs r4, r0 @ fd for memmap
+    MOVS r4, r0 @ fd for memmap
     @ check for error and print error msg if necessary
     BPL 1f @ pos number file opened ok
     MOV R1, #1 @ stdout
@@ -24,18 +24,19 @@
     writeFile R1, memOpnErr, R2 @ print the error
     B _end
 @ Set up can call the mmap2 Linux service
-1: ldr r5, =gpioaddr @ address we want / 4096
-    ldr r5, [r5] @ load the address
-    mov r1, #pagelen @ size of mem we want
+1:
+    LDR r5, =\phys_address @ address we want / 4096
+    LDR r5, [r5] @ load the address
+    MOV r1, #pagelen @ size of mem we want
 
 @ mem protection options
-    mov r2, #(PROT_READ + PROT_WRITE)
-    mov r3, #MAP_SHARED @ mem share options
-    mov r0, #0 @ let linux choose a virtual address
+    MOV r2, #(PROT_READ + PROT_WRITE) @ protectOptions
+    MOV r3, #MAP_SHARED @ mem share options
+    MOV r0, #0 @ let linux choose a virtual address
     
-    mov r7, #sys_mmap2 @ mmap2 service num
-    svc 0 @ call service
-    movs r8, r0 @ keep the returned virt addr
+    MOV r7, #sys_mmap2 @ mmap2 service num
+    SVC 0 @ call service
+    MOVS r8, r0 @ keep the returned virt addr
     @ check for error and print error msg
     @ if necessary.
     BPL 2f @ pos number file opened ok
@@ -53,46 +54,46 @@
 @ First is input time to sleep in seconds and nanoseconds.
 @ Second is time left to sleep if interrupted (which we ignore)
 .macro nanoSleep
-    ldr r0, =timespecsec
-    ldr r1, =timespecsec
-    mov r7, #sys_nanosleep
-    svc 0
+    LDR r0, =timespecsec
+    LDR r1, =timespecsec
+    MOV r7, #sys_nanosleep
+    SVC 0
 .endm
 
 .macro GPIODirectionOut pin
-    ldr r2, =\pin @ offset of select register
-    ldr r2, [r2] @ load the value
-    ldr r1, [r8, r2] @ address of register
-    ldr r3, =\pin @ address of pin table
-    add r3, #4 @ load amount to shift from table
-    ldr r3, [r3] @ load value of shift amt
-    mov r0, #0b111 @ mask to clear 3 bits
-    lsl r0, r3 @ shift into position
-    bic r1, r0 @ clear the three bits
-    mov r0, #1 @ 1 bit to shift into pos
-    lsl r0, r3 @ shift by amount from table
-    orr r1, r0 @ set the bit
-    str r1, [r8, r2] @ save it to reg to do work
+    LDR r2, =\pin @ offset of select register
+    LDR r2, [r2] @ load the value
+    LDR r1, [r8, r2] @ address of register
+    LDR r3, =\pin @ address of pin table
+    ADD r3, #4 @ load amount to shift from table
+    LDR r3, [r3] @ load value of shift amt
+    MOV r0, #0b111 @ mask to clear 3 bits
+    LSL r0, r3 @ shift into position
+    BIC r1, r0 @ clear the three bits
+    MOV r0, #1 @ 1 bit to shift into pos
+    LSL r0, r3 @ shift by amount from table
+    ORR r1, r0 @ set the bit
+    STR r1, [r8, r2] @ save it to reg to do work
 .endm
 
 .macro GPIOTurnOn pin, value
-    mov r2, r8 @ address of gpio regs
-    add r2, #setregoffset @ off to set reg
-    mov r0, #1 @ 1 bit to shift into pos
-    ldr r3, =\pin @ base of pin info table
-    add r3, #8 @ add offset for shift amt
-    ldr r3, [r3] @ load shift from table
-    lsl r0, r3 @ do the shift
-    str r0, [r2] @ write to the register
+    MOV r2, r8 @ address of gpio regs
+    ADD r2, #setregoffset @ off to set reg
+    MOV r0, #1 @ 1 bit to shift into pos
+    LDR r3, =\pin @ base of pin info table
+    ADD r3, #8 @ ADD offset for shift amt
+    LDR r3, [r3] @ load shift from table
+    LSL r0, r3 @ do the shift
+    STR r0, [r2] @ write to the register
 .endm
 
 .macro GPIOTurnOff pin, value
-    mov r2, r8 @ address of gpio regs
-    add r2, #clrregoffset @ off set of clr reg
-    mov r0, #1 @ 1 bit to shift into pos
-    ldr r3, =\pin @ base of pin info table
-    add r3, #8 @ add offset for shift amt
-    ldr r3, [r3] @ load shift from table
-    lsl r0, r3 @ do the shift
-    str r0, [r2] @ write to the register
+    MOV r2, r8 @ address of gpio regs
+    ADD r2, #clrregoffset @ off set of clr reg
+    MOV r0, #1 @ 1 bit to shift into pos
+    LDR r3, =\pin @ base of pin info table
+    ADD r3, #8 @ ADD offset for shift amt
+    LDR r3, [r3] @ load shift from table
+    LSL r0, r3 @ do the shift
+    STR r0, [r2] @ write to the register
 .endm
--- problema_1/main.s
+++ problema_1/main.s
@@ -34,7 +34,7 @@ memMapErr: .asciz "Failed to map memory\n"
 memMapsz: .word .-memMapErr
 accessFlags: .word O_RDWR + O_SYNC @ 00000002 + 00010000
 accessMode: .word S_RDWR @0666
-protectOptions: .word PROT_READ + PROT_WRITE 
+@protectOptions: .word PROT_READ + PROT_WRITE 
 
 @ mem address of gpio register / 4096
 gpioaddr: .word 0x20200 @ 0x7E20100
@@ -53,39 +53,11 @@ pin27: .word 8 @ offset to select register
 .global _start
 
 _start:
-    openFile devmem @ open /dev/mem
-    movs r4, r0 @ fd for memmap
-    @ check for error and print error msg if necessary
-    BPL 1f @ pos number file opened ok
-    MOV R1, #1 @ stdout
-    LDR R2, =memOpnsz @ Error msg
-    LDR R2, [R2]
-    writeFile R1, memOpnErr, R2 @ print the error
-    B _end
-@ Set up can call the mmap2 Linux service
-1: ldr r5, =gpioaddr @ address we want / 4096
-    ldr r5, [r5] @ load the address
-    mov r1, #pagelen @ size of mem we want
-
-@ mem protection options
-    ldr r2, =protectOptions @(PROT_READ + PROT_WRITE)
-    mov r3, #MAP_SHARED @ mem share options
-    mov r0, #0 @ let linux choose a virtual address
-    
-    mov r7, #sys_mmap2 @ mmap2 service num
-    svc 0 @ call service
-    movs r8, r0 @ keep the returned virt addr
-    @ check for error and print error msg
-    @ if necessary.
-    BPL 2f @ pos number file opened ok
-    MOV R1, #1 @ stdout
-    LDR R2, =memMapsz @ Error msg
-    LDR R2, [R2]
-    writeFile R1, memMapErr, R2 @ print the error
-    B _end
-2:
+    @ R8 will contain the virtual address of the peripheral physical address passed 
+    mapMem gpioaddr 
 
 _end: 
+    @ branch used by mapMem macro
     mov R0, #0 @ Use 0 return code
     mov R7, #1 @ Command code 1 terms
     svc 0 @ Linux command to terminate
