7f27fb03ceef84d1a90b35c19a125f6a08b3b30e
Gustavo Boanerges <g.boanerges2@gmail.com>
2022-04-20 12:32:17 -0300

    Clear unused constants,add settings to init uart

--- problema_1/main.s
+++ problema_1/main.s
@@ -1,64 +1,166 @@
+@============================================================================@
 @
-.include "gpiomem.s" @ already imports fileio.s
+@@ offsets to the UART registers
 @
+.equ UART_DR, 0x00 @ data register
+.equ UART_FR, 0x18 @ flag register
+.equ UART_IBRD, 0x24 @ integer baud rate divisor
+.equ UART_FBRD, 0x28 @ fractional baud rate divisor
+.equ UART_LCRH, 0x2C @ line control register
+.equ UART_CR, 0x30 @ control register
+@
+@@ Offsets of the Flag Register
+@
+.equ UART_TXFE, (1<<7) @ Transmit FIFO empty
+.equ UART_RXFF, (1<<6) @ Receive FIFO full
+.equ UART_TXFF, (1<<5) @ Transmit FIFO full
+.equ UART_RXFE, (1<<4) @ Receive FIFO empty
+.equ UART_BUSY, (1<<3) @ UART is busy xmitting
+@============================================================================@
+@
+@@ Bits for the CR (control register)
+@
+.equ UART_RXE, (1<<9) @ Enable receiver
+.equ UART_TXE, (1<<8) @ Enable transmitter
+.equ UART_LBE, (1<<7) @ Enable loopback
 
+.equ UART_FEN, (1<<4)
+
+.equ UART_UARTEN_ON, (1<<0) @ Enable UART
+.equ UART_UARTEN_OFF, (0<<0) @ Enable UART
+@============================================================================@
+@
+@@ Constants for the mmap2 function
+@
 .equ pagelen, 4096
-.equ setregoffset, 28
-.equ clrregoffset, 40
 .equ PROT_READ, 1
 .equ PROT_WRITE, 2
 .equ MAP_SHARED, 1
+@============================================================================@
 @
-.EQU O_RDONLY, 0
-.EQU O_WRONLY, 1
-.EQU O_CREAT, 0100
+@@ Constants to open the memory file
+@
 .EQU O_RDWR, 00000002
 .EQU O_SYNC, 00010000
-.EQU S_RDWR, 0666
-# linux directives
-.EQU sys_read, 3 @ read from a file descriptor
-.EQU sys_write, 4 @ write to a file descriptor
+@============================================================================@
+@
+@@ Linux directives 
+@
 .EQU sys_open, 5 @ open and possibly create a file
-.EQU sys_close, 6 @ close a file descriptor
-.EQU sys_mmap2, 192 @ map files or devices
+.EQU sys_mmap2, 192 @ function mmap2 to map the physical address
+@============================================================================@
+@
+@@ Labels used within the program
 @
 .align 2
 .section .data
-timespecsec: .word 0
-timespecnano: .word 100000000
 devmem: .asciz "/dev/mem"
-memOpnErr: .asciz "Failed to open /dev/mem\n"
-memOpnsz: .word .-memOpnErr
-memMapErr: .asciz "Failed to map memory\n"
-memMapsz: .word .-memMapErr
 accessFlags: .word O_RDWR + O_SYNC @ 00000002 + 00010000
-accessMode: .word S_RDWR @0666
 protectOptions: .word PROT_READ + PROT_WRITE 
-
-@ mem address of gpio register / 4096
-gpioaddr: .word 0x20200 
-uartaddr: .word 0xbee1e100 @ 0x7E20100
-pin17: .word 4 @ offset to select register
- .word 21 @ bit offset in select register
- .word 17 @ bit offset in set & clr register
-pin22: .word 8 @ offset to select register
- .word 6 @ bit offset in select register
- .word 22 @ bit offset in set & clr register
-pin27: .word 8 @ offset to select register
- .word 21 @ bit offset in select register
- .word 27 @ bit offset in set & clr register
-
+uart_address: .word 0x20201 @ 0x7E20100
+@============================================================================@
 .section .text
 	 .align 2
 .global _start
 
 _start:
-    @ R8 will contain the virtual address of the peripheral physical address passed 
-    mapMem gpioaddr 
-    
-_end: 
-    @ branch used by mapMem macro
-    MOV R0, #0 @ Use 0 return code
-    MOV R7, #1 @ Command code 1 terms
-    SVC 0 @ Linux command to terminate
+@============================================================================@
+@
+@@ Open the dev mem file to map the physical address we want
+@ 
+    LDR r0, =devmem
+    LDR r1, =accessFlags @ =(O_RDWR + O_SYNC)
+    MOV r7, #sys_open
+    SVC 0
+@
+@@ End open file
+@
+@============================================================================@
+@
+@@ Setting up the parameters needed to call the mmap2 Linux service
+@@ that in fact will map the physical address into virtual address
+@
+    MOVS r4, r0 @ fd for memmap function (fd stands for - File Descriptor)
+                @ copying the content from r0 to r4 
+    LDR r5, =uart_address @ load the address of the label uart_address
+    LDR r5, [r5] @ load the uart address we want already divided by 4096
+    MOV r1, #pagelen @ size of mem we want
 
+    @ mem protection options
+    LDR r2, =protectOptions @ mov r2, #(PROT_READ + PROT_WRITE)
+    MOV r3, #MAP_SHARED @ mem share options
+    MOV r0, #0 @ let linux choose a virtual address
+    
+    MOV r7, #sys_mmap2 @ mmap2 service num
+    SVC 0 @ call service and after that r0 will have the mapped address of uart
+    MOVS r8, r0 @ keep the returned virt address
+                @ R8 now contains the virtual address
+@============================================================================@
+@
+@@ UART Configuration 
+@@ UARTEN bit on the Control Register must be set to 1, to activate the UART.
+@@ But first we must disable the UART to change settings.
+@@ Wait for the end of transmission or reception of the current character.
+@@ Flush the transmit FIFO by setting the FEN bit to 0 in the Line Control Register.
+@@ Reprogram the Control Register
+@@ Enable the UART.
+@               
+    MOV r0, #UART_UARTEN_OFF @ disable uart to begin changing settings
+    STR r0,[r1, #UART_CR]    @ storing the value 0 to the first bit position 
+                             @ which is the bit UARTEN
+
+@ Now we have to check if the there is a transmission or reception of data
+fifo_status: @ looping until the there is no data i/o
+    LDR r0, [r8, #UART_FR] @ load the flag register 
+    ANDS r0, #UART_RXFE @ 'AND and Set Flags' operation to check if the bit on the position of 
+                        @ the receive FIFO is empty == 1, if its not branch back
+                        @ to check again until the bit is == 1                     
+    @ CMP r0, #0
+    @ CBNZ r0, fifo_status
+    BNE fifo_status
+
+
+@ Disabling FIFO on the Line Control Register
+    MOV r0, #UART_FEN # 1 << 4 which is the bit position of the FIFO enable bit
+    STR r0, [r8, #UART_LCRH]
+    @LDR r2, [r8, #UART_LCRH]
+    @BIC r2, r0
+    @STR r2, [r8, #UART_LCRH]
+    
+
+@ Configure the Line Control Register with the following options:
+@ BITS 6 5 4 3 2 1 0
+@      1 0 0 1 1 1 0
+@ Parity enabled
+@ Even Parity
+@ Two Stop Bits
+@ 7 bits of message size
+
+@ Configure Baud Rate
+    MOV r0, #integervalue
+    STR r0, [r8, #UART_IBRD]
+    MOV r0, #fractionalvalue
+    STR r0, [r8, #UART_FBRD]
+
+
+@ Enable FIFO
+    MOV r0, #UART_FEN # 1 << 4 which is the bit position of the FIFO enable bit
+    LDR r1, [r8, #UART_LCRH]
+    ORR r1, r0 @ OR operation to maintain the configuration of the message  
+               @ now reactivating the FIFO, bit position to 1
+    STR r1, [r8, #UART_LCRH] 
+
+@ Enable UART again
+    MOV r0, #UART_UARTEN_ON @ enabling uart 
+    STR r0,[r1, #UART_CR]  
+
+
+@============================================================================@
+@
+@@ End the execution
+@
+Exit:
+    MOV r0, #0
+    MOV r7, #1
+    SVC 0
+@============================================================================@
