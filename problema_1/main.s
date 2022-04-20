40883e09d8bae2d8e07be33b20186f8fdbc8e3c6
Gustavo Boanerges <g.boanerges2@gmail.com>
2022-04-20 19:11:11 -0300

    Remove unused files, clear main comments

--- problema_1/main.s
+++ problema_1/main.s
@@ -1,13 +1,20 @@
 @============================================================================@
 @
-@@ offsets to the UART registers
+@@ offsets of the UART registers
 @
-.equ UART_DR, 0x00 @ data register
-.equ UART_FR, 0x18 @ flag register
-.equ UART_IBRD, 0x24 @ integer baud rate divisor
-.equ UART_FBRD, 0x28 @ fractional baud rate divisor
-.equ UART_LCRH, 0x2C @ line control register
+.equ UART_DR, 0x00 @ data register 0
+.equ UART_FR, 0x18 @ flag register 24
+.equ UART_IBRD, 0x24 @ integer baud rate divisor 36
+.equ UART_FBRD, 0x28 @ fractional baud rate divisor40
+.equ UART_LCRH, 0x2C @ line control register 44
 .equ UART_CR, 0x30 @ control register
+.equ UART_CONFIG_PARAMETERS, 0b1001110
+                    @ BITS 6 5 4 3 2 1 0
+                    @      1 0 0 1 1 1 0
+                    @ Parity enabled
+                    @ Even Parity
+                    @ Two Stop Bits
+                    @ 7 bits of message size
 @
 @@ Offsets of the Flag Register
 @
@@ -44,7 +51,7 @@
 .EQU O_SYNC, 00010000
 @============================================================================@
 @
-@@ Linux directives 
+@@ Linux directives
 @
 .EQU sys_open, 5 @ open and possibly create a file
 .EQU sys_mmap2, 192 @ function mmap2 to map the physical address
@@ -53,23 +60,23 @@
 @@ Labels used within the program
 @
 .align 2
-.section .data
+.data
 devmem: .asciz "/dev/mem"
-accessFlags: .word O_RDWR + O_SYNC @ 00000002 + 00010000
-protectOptions: .word PROT_READ + PROT_WRITE 
+protectOptions: .word PROT_READ + PROT_WRITE
 uart_address: .word 0x20201 @ 0x7E20100
 @============================================================================@
+.align 2
+
 .section .text
-	 .align 2
 .global _start
 
 _start:
 @============================================================================@
 @
 @@ Open the dev mem file to map the physical address we want
-@ 
+@
     LDR r0, =devmem
-    LDR r1, =accessFlags @ =(O_RDWR + O_SYNC)
+    LDR r1, =(O_RDWR + O_SYNC)
     MOV r7, #sys_open
     SVC 0
 @
@@ -81,7 +88,7 @@ _start:
 @@ that in fact will map the physical address into virtual address
 @
     MOVS r4, r0 @ fd for memmap function (fd stands for - File Descriptor)
-                @ copying the content from r0 to r4 
+                @ copying the content from r0 to r4
     LDR r5, =uart_address @ load the address of the label uart_address
     LDR r5, [r5] @ load the uart address we want already divided by 4096
     MOV r1, #pagelen @ size of mem we want
@@ -90,43 +97,40 @@ _start:
     LDR r2, =protectOptions @ mov r2, #(PROT_READ + PROT_WRITE)
     MOV r3, #MAP_SHARED @ mem share options
     MOV r0, #0 @ let linux choose a virtual address
-    
+
     MOV r7, #sys_mmap2 @ mmap2 service num
     SVC 0 @ call service and after that r0 will have the mapped address of uart
     MOVS r8, r0 @ keep the returned virt address
                 @ R8 now contains the virtual address
+@ close the devmem file
+    MOV r0, r4
+    MOV r7, #6
+    SVC 0
 @============================================================================@
 @
-@@ UART Configuration 
+@@ UART Configuration
 @@ UARTEN bit on the Control Register must be set to 1, to activate the UART.
 @@ But first we must disable the UART to change settings.
 @@ Wait for the end of transmission or reception of the current character.
 @@ Flush the transmit FIFO by setting the FEN bit to 0 in the Line Control Register.
 @@ Reprogram the Control Register
 @@ Enable the UART.
-@               
-    MOV r0, #UART_UARTEN_OFF @ disable uart to begin changing settings
-    STR r0,[r1, #UART_CR]    @ storing the value 0 to the first bit position 
+@
+    MOV r0, #0 @ disable uart to begin changing settings
+    STR r0, [r8, #UART_CR]    @ storing the value 0 to the first bit position
                              @ which is the bit UARTEN
 
 @ Now we have to check if the there is a transmission or reception of data
 fifo_status: @ looping until the there is no data i/o
-    LDR r0, [r8, #UART_FR] @ load the flag register 
-    ANDS r0, #UART_RXFE @ 'AND and Set Flags' operation to check if the bit on the position of 
-                        @ the receive FIFO is empty == 1, if its not branch back
-                        @ to check again until the bit is == 1                     
-    @ CMP r0, #0
-    @ CBNZ r0, fifo_status
+    LDR r0, [r8, #UART_FR] @ load the flag register
+    TST r0, #UART_TXFF @ r0 AND TXFF (0b10000)
     BNE fifo_status
 
-
 @ Disabling FIFO on the Line Control Register
-    MOV r0, #UART_FEN # 1 << 4 which is the bit position of the FIFO enable bit
-    STR r0, [r8, #UART_LCRH]
-    @LDR r2, [r8, #UART_LCRH]
-    @BIC r2, r0
-    @STR r2, [r8, #UART_LCRH]
-    
+    MOV r0, #UART_FEN @ 1 << 4 which is the bit position of the FIFO enable bit
+    LDR r2, [r8, #UART_LCRH] @ 11110000 AND 10000
+    BIC r2, r0 @ r2 AND r0 - so all bits from r2 will get a 0, except the one in r0
+    STR r2, [r8, #UART_LCRH] 
 
 @ Configure the Line Control Register with the following options:
 @ BITS 6 5 4 3 2 1 0
@@ -135,31 +139,34 @@ fifo_status: @ looping until the there is no data i/o
 @ Even Parity
 @ Two Stop Bits
 @ 7 bits of message size
+    MOV r0, #UART_CONFIG_PARAMETERS @ #0b1001110
+    LDR r1, [r8, #UART_LCRH]
+    BIC r1, r0
+    STR r1, [r8, #UART_LCRH]
 
 @ Configure Baud Rate
-    MOV r0, #integervalue
+@(3MHz / ( 115200 ∗ 16 )) = 1.62760416667
+    MOV r0, #1
     STR r0, [r8, #UART_IBRD]
-    MOV r0, #fractionalvalue
+    MOV r0, #0x3e
     STR r0, [r8, #UART_FBRD]
 
-
 @ Enable FIFO
-    MOV r0, #UART_FEN # 1 << 4 which is the bit position of the FIFO enable bit
-    LDR r1, [r8, #UART_LCRH]
-    ORR r1, r0 @ OR operation to maintain the configuration of the message  
+    MOV r0, #UART_FEN @ (1 << 4) = 10000
+                      @ which is the bit position of the FIFO enable bit
+    LDR r1, [r8, #UART_LCRH] @     1001110
+    ORR r1, r0 @ OR operation to maintain the configuration of the message
                @ now reactivating the FIFO, bit position to 1
-    STR r1, [r8, #UART_LCRH] 
+    STR r1, [r8, #UART_LCRH]
 
-@ Enable UART again
-    MOV r0, #UART_UARTEN_ON @ enabling uart 
-    STR r0,[r1, #UART_CR]  
+@ Enable UART again and TX and RX
+    MOV r0, #UART_UARTEN_ON @ enabling uart bit 1 on UARTEN
+    MOV r2, #0b11 @ TXE and RXE bits
+    LSL r2, #8 @ shifting 8 bits 1100000000
+    ADD r0, r0, r2
+    STR r0, [r8, #UART_CR]
 
-
-@============================================================================@
-@
-@@ End the execution
-@
-Exit:
+@ End the execution
     MOV r0, #0
     MOV r7, #1
     SVC 0
